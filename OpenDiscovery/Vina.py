affdb48927510eed969ef2f6a035f815b9e726db
Gareth Price <gareth.price@warwick.ac.uk>
2014-01-27 21:34:20 +0000

    fixed linux bug

--- OpenDiscovery/Vina.py
+++ OpenDiscovery/Vina.py
@@ -24,13 +24,13 @@ class Vina(object):
 		elif 'darwin' in sys.platform:
 			self.locations['vina'] = screen.protocol_dir + "/OpenDiscovery/lib/vina-osx/vina"
 
-			self.locations['receptor'] = screen.ligand_dir + "/receptor/" + screen.options['receptor'] + ".pdbqt"
-			self.locations['ligand'] = screen.ligand_dir + "/ligands/" + cmpnd + ".pdbqt"
-			self.locations['config'] = screen.ligand_dir + "/receptor/" + screen.options['receptor'] + ".txt"
-			self.locations['results'] = screen.ligand_dir + "/results/" + cmpnd + ".pdbqt"
-			self.locations['log'] = screen.ligand_dir + "/results/" + cmpnd + ".txt"
+		self.locations['receptor'] = screen.ligand_dir + "/receptor/" + screen.options['receptor'] + ".pdbqt"
+		self.locations['ligand'] = screen.ligand_dir + "/ligands/" + cmpnd + ".pdbqt"
+		self.locations['config'] = screen.ligand_dir + "/receptor/" + screen.options['receptor'] + ".txt"
+		self.locations['results'] = screen.ligand_dir + "/results/" + cmpnd + ".pdbqt"
+		self.locations['log'] = screen.ligand_dir + "/results/" + cmpnd + ".txt"
 
-			makeFolder(screen.ligand_dir + "/results")
+		makeFolder(screen.ligand_dir + "/results")
 
 	def run(self):
 		subprocess.call('{vina} --receptor {receptor} --ligand {ligand} --config {conf} --out {results} --log {log} --exhaustiveness {exhaustiveness}'.format(

384884861a5668613646e9b6acbaa841534efea8
Gareth Price <gareth.price@warwick.ac.uk>
2014-01-28 01:20:35 +0000

    changing odscreen to parser

--- odscreen.py
+++ odscreen.py
@@ -9,19 +9,19 @@ from time import gmtime, strftime, time
 if __name__ == '__main__':
 
 	# Set up parser for options via command line
-	#parser = argparse.ArgumentParser(description='Open Discovery Screening Protocol')
-	#parser.add_argument('-d', '--directory', help='Path to the ligand directory. Required!', required=True)
-	#parser.add_argument('-e', '--exhaustiveness', help='Exhaustiveness. Default = 20.', type=int, default=20)
-	#parser.add_argument('-r', '--receptor', help='Receptor Name. Must be located within the receptor folder. Default = receptor.', default='receptor')
-	#parser.add_argument('--driver', help='The docking driver to use. Default = vina, default='vina')
-	#options = vars(parser.parse_args())
+	parser = argparse.ArgumentParser(description='Open Discovery Screening Protocol')
+	parser.add_argument('-d', '--directory', help='Path to the ligand directory. Required!', required=True)
+	parser.add_argument('-e', '--exhaustiveness', help='Exhaustiveness. Default = 20.', type=int, default=20)
+	parser.add_argument('-r', '--receptor', help='Receptor Name. Must be located within the receptor folder. Default = receptor.', default='receptor')
+	parser.add_argument('--driver', help='The docking driver to use. Default = vina', default='vina')
+	options = vars(parser.parse_args())
 
 	# or hard code them in
-	options                   = {}
-	options['directory']      = '~/Desktop/od2/'
-	options['receptor']       = 'receptor'
-	options['exhaustiveness'] = 1
-	options['driver'] 		  = 'vina'
+	#options                   = {}
+	#options['directory']      = '~/Desktop/od2/'
+	#options['receptor']       = 'receptor'
+	#options['exhaustiveness'] = 1
+	#options['driver'] 		  = 'vina'
 
 	# let's time it
 	t = time()
